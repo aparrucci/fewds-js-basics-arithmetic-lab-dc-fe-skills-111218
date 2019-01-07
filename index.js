4781f4fca988f7459569d43bcf2c7ad5712bcdf5
Andrew Parrucci <am.parrucci@gmail.com>
2019-01-07 02:33:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ let newID = oldID + 1000000000;
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.random(0,20);
 let randomInteger = Math.floor(randomNumber);
-let randomUserID = Math.random((randomInteger) * 1) + 100000000;
+let randomUserID = Math.random(randomInteger);
